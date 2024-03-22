c3e3dfc3e9df4903184650e85ec39a723dbe507b
Shavidika <shavidika.ekanayake@gmail.com>
2024-03-22 11:59:09 +0530

    remove comments

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import login_signup.Login;
 
 public class Main {
     public static void main(String[] args) {
-//hjson
+        
         Login LoginFrame = new Login();
         LoginFrame.setVisible(true);
         LoginFrame.pack();
         LoginFrame.setLocationRelativeTo(null);
-        //jhjh
-        //hjj
     }
 }
